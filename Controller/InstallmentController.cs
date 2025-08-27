f8f2a36ed952143b54e34e5fb2eab9f74266bbdb
kamolovv11 <kmlvbilol@gmail.com>
2025-08-28 01:33:01 +0500

    Added Controller and changed Program.cs

---
+++ Controller/InstallmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Infrastructure;
+
+namespace WebApplication1.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class InstallmentController(InstallmentService installmentService) : ControllerBase
+{
+    [HttpGet("installment")]
+    public ActionResult<string> Installment(string product, int price, string phoneNumber, int installment)
+    {
+        try
+        {
+            return Ok(installmentService.Installments(product, price, phoneNumber, installment));
+        }
+        catch (ArgumentException e)
+        {
+            return BadRequest(e.Message);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+}
